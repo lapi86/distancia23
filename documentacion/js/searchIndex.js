c55a957b2995cf420d1b9a757189bdb50fa49631
lapi86 <132487827+lapi86@users.noreply.github.com>
2023-05-03 19:41:09 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020para\u0020sumar\u0020dos\u0020n\u00FAmeros\u0020enteros",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\multiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "Funci\u00F3n\u0020para\u0020multiplicar\u0020dos\u0020n\u00FAmeros\u0020enteros",
+            "url": "namespaces/default.html#function_multiplicar"
+        },                {
+            "fqsen": "\\ManejadorArchivos",
+            "name": "ManejadorArchivos",
+            "summary": "Clase\u0020para\u0020manejo\u0020de\u0020archivos",
+            "url": "classes/ManejadorArchivos.html"
+        },                {
+            "fqsen": "\\ManejadorArchivos\u003A\u003AleerArchivo\u0028\u0029",
+            "name": "leerArchivo",
+            "summary": "M\u00E9todo\u0020para\u0020leer\u0020el\u0020contenido\u0020de\u0020un\u0020archivo",
+            "url": "classes/ManejadorArchivos.html#method_leerArchivo"
+        },                {
+            "fqsen": "\\ManejadorArchivos\u003A\u003AescribirArchivo\u0028\u0029",
+            "name": "escribirArchivo",
+            "summary": "M\u00E9todo\u0020para\u0020escribir\u0020en\u0020un\u0020archivo",
+            "url": "classes/ManejadorArchivos.html#method_escribirArchivo"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
