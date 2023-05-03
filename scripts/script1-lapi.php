fdeaf37b0aa95773cc5f7e70c0edc9c305b094c3
lapi86 <132487827+lapi86@users.noreply.github.com>
2023-05-03 19:40:25 +0200

    Add files via upload

---
+++ scripts/script1-lapi.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Este script contiene dos funciones para operaciones matemáticas básicas
+ * @author Manuel Lapi García
+ * @version 1.0
+ */
+
+/**
+ * Función para sumar dos números enteros
+ *
+ * @param int $a Primer número entero
+ * @param int $b Segundo número entero
+ * @return int Resultado de la suma
+ * @since 1.0
+ */
+function sumar($a, $b) {
+  return $a + $b;
+}
+
+/**
+ * Función para multiplicar dos números enteros
+ *
+ * @param int $a Primer número entero
+ * @param int $b Segundo número entero
+ * @return int Resultado de la multiplicación
+ * @since 1.0
+ */
+function multiplicar($a, $b) {
+  return $a * $b;
+}
+
+?>
---
+++ scripts/script2-lapi.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Este script contiene una clase con dos métodos para manejo de archivos
+ * @author Manuel Lapi García
+ * @version 1.0
+ */
+
+/**
+ * Clase para manejo de archivos
+ *
+ * @since 1.0
+ */
+class ManejadorArchivos {
+  
+  /**
+   * Método para leer el contenido de un archivo
+   *
+   * @access public
+   * @param string $ruta Ruta del archivo a leer
+   * @return string Contenido del archivo leído
+   * @since 1.0
+   */
+  public function leerArchivo($ruta) {
+    $archivo = fopen($ruta, "r");
+    $contenido = fread($archivo, filesize($ruta));
+    fclose($archivo);
+    return $contenido;
+  }
+  
+  /**
+   * Método para escribir en un archivo
+   *
+   * @access public
+   * @param string $ruta Ruta del archivo donde escribir
+   * @param string $contenido Contenido a escribir en el archivo
+   * @return bool true si se escribió correctamente, false en caso contrario
+   * @since 1.0
+   */
+  public function escribirArchivo($ruta, $contenido) {
+    $archivo = fopen($ruta, "w");
+    $resultado = fwrite($archivo, $contenido);
+    fclose($archivo);
+    return $resultado !== false;
+  }
+
+}
+?>
